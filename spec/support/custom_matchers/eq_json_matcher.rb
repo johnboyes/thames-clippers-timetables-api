8d28253c51458036162e17863ca3ef0b1491cdea
John Boyes <john@edinburghwebservices.co.uk>
2019-03-16 08:03:23 +0000

    Improve failure message on failing test

--- spec/support/custom_matchers/eq_json_matcher.rb
+++ spec/support/custom_matchers/eq_json_matcher.rb
@@ -1,6 +1,11 @@
 RSpec::Matchers.define :eq_json do |expected|
-  match do |response|
-    response_body = JSON.parse(response.body)
-    (response_body == expected) && (response.status == 200)
+  expected_status_code = 200
+  match do |actual|
+    response_body = JSON.parse(actual.body)
+    (response_body == expected) && (actual.status == expected_status_code)
+  end
+  failure_message do |actual|
+    "expected that #{actual.body} would equal #{expected}
+     and that status code #{actual.status} would equal #{expected_status_code}"
   end
 end
