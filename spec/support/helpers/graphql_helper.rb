0a4e649f4a11d121664cc152b10149bb46313551
John Boyes <john@edinburghwebservices.co.uk>
2019-03-16 10:31:24 +0000

    Rename test method to make intent clearer

--- spec/support/helpers/graphql_helper.rb
+++ spec/support/helpers/graphql_helper.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 # Helper methods for GraphQL specs
 module GraphQLHelpers
-  def execute_graphql_query(query)
+  def request_sailing_times(query)
     post '/graphql', params: { 'query' => query }
   end
 end
