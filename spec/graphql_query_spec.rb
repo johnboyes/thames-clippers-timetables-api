f29f7a249c9d81ffd2a346bcfc08dbb070a2e5c2
John Boyes <john@edinburghwebservices.co.uk>
2019-03-18 19:20:30 +0000

    Retrieve real route times

--- spec/graphql_query_spec.rb
+++ spec/graphql_query_spec.rb
@@ -27,14 +27,29 @@ RSpec.describe 'graphql', type: :request do
             'from' => 'WANDSWORTH_RIVERSIDE_QUARTER_PIER',
             'to' => 'BLACKFRIARS_PIER',
             'sailings' => [
-              {
-                'departureTime' => '0710',
-                'arrivalTime' => '0744'
-              },
-              {
-                'departureTime' => '0805',
-                'arrivalTime' => '0839'
-              }
+              { 'departureTime' => '0559', 'arrivalTime' => '0639' },
+              { 'departureTime' => '0617', 'arrivalTime' => '0656' },
+              { 'departureTime' => '0647', 'arrivalTime' => '0729' },
+              { 'departureTime' => '0717', 'arrivalTime' => '0759' },
+              { 'departureTime' => '0748', 'arrivalTime' => '0822' },
+              { 'departureTime' => '0802', 'arrivalTime' => '0841' },
+              { 'departureTime' => '0832', 'arrivalTime' => '0911' },
+              { 'departureTime' => '0905', 'arrivalTime' => '0945' },
+              { 'departureTime' => '0934', 'arrivalTime' => '1020' },
+              { 'departureTime' => '0952', 'arrivalTime' => '1038' },
+              { 'departureTime' => '1722', 'arrivalTime' => '1808' },
+              { 'departureTime' => '1757', 'arrivalTime' => '1838' },
+              { 'departureTime' => '1827', 'arrivalTime' => '1907' },
+              { 'departureTime' => '1847', 'arrivalTime' => '1927' },
+              { 'departureTime' => '1907', 'arrivalTime' => '1947' },
+              { 'departureTime' => '1927', 'arrivalTime' => '2007' },
+              { 'departureTime' => '1947', 'arrivalTime' => '2027' },
+              { 'departureTime' => '2002', 'arrivalTime' => '2042' },
+              { 'departureTime' => '2017', 'arrivalTime' => '2057' },
+              { 'departureTime' => '2032', 'arrivalTime' => '2112' },
+              { 'departureTime' => '2052', 'arrivalTime' => '2132' },
+              { 'departureTime' => '2117', 'arrivalTime' => '2157' },
+              { 'departureTime' => '2137', 'arrivalTime' => '2217' }
             ]
           }
       }
