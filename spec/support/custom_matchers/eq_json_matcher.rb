3f704cb043b5c2ef3ef80eb59331438bd79f53af
John Boyes <john@edinburghwebservices.co.uk>
2019-09-14 17:57:37 +0100

    Improve test failure message readability

--- spec/support/custom_matchers/eq_json_matcher.rb
+++ spec/support/custom_matchers/eq_json_matcher.rb
@@ -5,7 +5,11 @@ RSpec::Matchers.define :eq_json do |expected|
     (response_body == expected) && (actual.status == expected_status_code)
   end
   failure_message do |actual|
-    "expected that #{actual.body} would equal #{expected}
+    "expected that #{pretty(JSON.parse(actual.body))} would equal #{pretty(expected)}
      and that status code #{actual.status} would equal #{expected_status_code}"
   end
+end
+
+def pretty(json)
+  JSON.pretty_generate(json)
 end
